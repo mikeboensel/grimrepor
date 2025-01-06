a8ae95b6b93262fbc30e7c3dc9a16e94cc582f0e
mikeboensel-macbook <mikeboensel@users.noreply.github.com>
2025-01-05 22:48:03 -0500

    Simpler method of determining Docker containerization for the time being

--- utils/env.py
+++ utils/env.py
@@ -1,6 +1,13 @@
+import os
+
 def is_docker():
+    '''
+    Check if the code is running in a docker container
+    May get a smarter check in the future. For now just relying on this 
+    variable only being set by Compose'''
     try:
-        with open("/proc/1/cgroup", "rt") as f:
-            return "docker" in f.read() or "containerd" in f.read()
+        ret = os.getenv("USING_DOCKER", False)
+        if ret:
+            return True # Easy way to convert str to bool
     except Exception:
         return False
